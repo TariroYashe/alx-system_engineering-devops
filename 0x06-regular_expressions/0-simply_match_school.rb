f90de2b95797c74e2a6a7d86caa4d74d8ec550e1
TariroYashe <rukodzac@gmail.com>
2023-11-28 13:43:34 +0200

    regular expression must match School

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+def match_school(argument)
+    regex = /\bSchool\b/
+    puts argument.scan(regex).join
+  end
+  
+  if ARGV.length != 1
+    puts "Usage: #{$PROGRAM_NAME} <string>"
+    exit 1
+  end
+  
+  match_school(ARGV[0])
+  
+  
