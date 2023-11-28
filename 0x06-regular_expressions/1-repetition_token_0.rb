961b10532d261ce50ee5b09f3974acae04c4ddd1
TariroYashe <rukodzac@gmail.com>
2023-11-28 14:05:29 +0200

    0x06-regular_expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,27 +1,19 @@
 #!/usr/bin/env ruby
 
-# regex_script.rb
-
-def match_input(input)
-    email_regex = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\z/
-    phone_regex = /\A\d{3}-\d{3}-\d{4}\z/
-  
-    if input.match?(email_regex)
-      puts "Input is a valid email address."
-    elsif input.match?(phone_regex)
-      puts "Input is a valid phone number."
+def match_first_line(input)
+    regex = /ruby/i # The 'i' flag makes the regex case-insensitive
+    if input =~ regex
+      puts "The first line contains 'ruby' in any case."
     else
-      puts "Input does not match any expected pattern."
+      puts "The first line does not contain 'ruby' in any case."
     end
   end
   
-  # Get the argument from the command line
-  input_argument = ARGV[0]
-  
   # Check if an argument is provided
-  if input_argument.nil?
-    puts "Please provide an argument."
+  if ARGV.empty?
+    puts "Usage: ruby script.rb <input>"
   else
-    match_input(input_argument)
+    input = File.open(ARGV[0]) { |f| f.readline } # Read the first line of the file
+    match_first_line(input)
   end
   
