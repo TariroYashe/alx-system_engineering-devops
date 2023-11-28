b0a0b5bbc889b572cbf457d0eda4921cedc0f19b
TariroYashe <rukodzac@gmail.com>
2023-11-28 14:24:15 +0200

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+def match_requirements(input)
+    regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/
+    if input.match?(regex)
+      puts "Input '#{input}' meets the requirements."
+    else
+      puts "Input '#{input}' does not meet the requirements."
+    end
+  end
+  
+  # Check if there is exactly one command-line argument
+  if ARGV.length == 1
+    input = ARGV[0]
+    match_requirements(input)
+  else
+    puts "Usage: ruby script.rb <input>"
+  end
+  
